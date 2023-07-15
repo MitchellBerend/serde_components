3efaaf3a284747b3f43509742ed79114561abafa
mitchellbhuysen@hotmail.com <mitchellbhuysen@hotmail.com>
2023-07-15 10:33:31 +0200

    Removed unused type def

--- serde_components/deserializers/__init__.py
+++ serde_components/deserializers/__init__.py
@@ -4,5 +4,3 @@ from .base import BaseDeserializer
 from .csv_deserializer import CsvDeserializer
 from .json_deserializer import JsonDeserializer
 from .toml_deserializer import TomlDeserializer
-
-D = TypeVar('D', bound=BaseDeserializer)
